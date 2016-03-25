de110b8ddec6db81995689ac831a69416370c308
Julien Aigret <julien.aigret@student.uclouvain.be>
2016-03-25 12:24:01 +0100

    commit auto

--- test.c
+++ test.c
@@ -7,13 +7,22 @@ typedef struct {
 } poly;
 
 double eval(poly *P, double x){
-  double Px = (((((((P->coeff[9]*x + P->coeff[8])*x + P->coeff[7])*x + P->coeff[6])*x + P->coeff[5])*x + P->coeff[4])*x + P->coeff[3])*x + P->coeff[2])*x + P->coeff[1])*x + P->coeff[0];
-  return Px;
+  double a_9 = P->coeff[9] ;
+  double a_8 = P->coeff[8] ;
+  double a_7 = P->coeff[7] ;
+  double a_6 = P->coeff[6] ;
+  double a_5 = P->coeff[5] ;
+  double a_4 = P->coeff[4] ;
+  double a_3 = P->coeff[3] ;
+  double a_2 = P->coeff[2] ;
+  double a_1 = P->coeff[1] ;
+  double a_0 = P->coeff[0] ;
+  return ((((((((a_9 * x + a_8) * x + a_7) * x + a_6) * x + a_5) * x + a_4) * x + a_3) * x + a_2) * x + a_1) * x + a_0) ;
 }
 
 poly *derivee(poly *P) {
   poly *ret = (poly *) malloc(sizeof(poly));
-  for (int 1 = 0; i < 9; i ++) {
+  for (int i = 0; i < 9; i ++) {
     ret->coeff[i] = (i+1) * P->coeff[i+1];
   }
   ret->coeff[9]=0;
